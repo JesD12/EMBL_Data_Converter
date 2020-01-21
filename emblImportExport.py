ea1c9ce821bbe6b890a65f6992a9d4726c4db5a1
Jes Dreier <fingernisk@hotmail.com>
2020-01-21 23:22:23 +0100

    Update to actully work -  check file folder structure

--- emblImportExport.py
+++ emblImportExport.py
@@ -2,30 +2,64 @@
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
+import csv
 
-# if not plt.isinteractive():
-#    plt.ion()
+def savetheimage(imagetosave,filename, category= 'Healthy',savepath = 'C:\\EMBLE data\\Data\\', valprocent = 0.2):
+    im = Image.fromarray(imagetosave)
+    #determine whether it should be saved as a training or validation
+    #if it need to be the same for nuc and actin put an output on this that
+    #function as an input next time this funtion is run
+    subfolder = 'TRAIN\\'
+    if np.random.uniform() < valprocent:
+        subfolder = 'VAL\\'
+    im.save(savepath+subfolder+category+'\\'+filename+'.png')
 
-path = 'c:\\EMBL data\\'
-filename = 'EMBL_FullData_3channel.hdf'
-print(path + filename)
+ImageFile = 'C:\\EMBL data\\Jes 3D 3C plate 2__2019-12-11T10_29_03-Measurement 1\\CellCogData\\EMBL_FullData_3channel.hdf'
+SavePath = 'C:\\EMBL data\\Data\\'
+csvpath = 'C:\\EMBL data\\Jes 3D 3C plate 2__2019-12-11T10_29_03-Measurement 1\\CellCogData\\TableData_full\\'
+csvfilename = 'EMBL_FullData_3channel_'
+Classifyerlist = ['Healthy', 'Binucleated', 'Big', 'Mitotic']
 
-with h5py.File(path + filename, 'r') as f:
-    # List all groups
-    print("Keys: %s" % f.keys())
-    a_group_key = list(f.keys())[0]
-
+with h5py.File(ImageFile, 'r') as f:
     image_data_set = f['data/gallery']
-    plt.imshow(image_data_set[:, :, 0:3, 0], cmap='gray')
-    plt.show()
-# )
-print(type(image_data_set))
-# plot the imagetest
-# image_test = image_data_set[:, :, :, 0][()]
+    #Set up a for loop to go over all 3 csv files
+    i = 0
+    missed = 0
+    for csvidx in range(1, 4):
+        with open(csvpath + csvfilename + f'{csvidx}of3.csv') as csvfile:
+            print(f'Starting on {csvidx} of 3')
+            csv_reader = csv.reader(csvfile,delimiter = ',')
+            #skip the header row
+            next(csv_reader,None)
+            for row in csv_reader:
+                #check whether the image is classified
+                if int(row[4]) in range(4):
+                    cat = Classifyerlist[int(row[4])]
+                    filename = f'Image_{i:05d}'
+                    #save the nuclues image
+                    savetheimage(image_data_set[:, :, 0, i],filename, category=cat, savepath=SavePath+'Nucleus\\')
+                    # saveActin
+                    savetheimage(image_data_set[:, :, 2, i],filename,category= cat, savepath=SavePath+'Actin\\')
+                else:
+                    missed += 1
+                #check-up
+                if i%500 == 0:
+                    print(f'ID: {row[0]} is {row[3]} and therefore a number: {row[4]}')
+                    print(f'determined as image number {i:05d} and put into the {cat} group')
+                    if missed > 0:
+                        print(f'currently we have skipped :{missed}')
+                i += 1
 
-# plt.imshow(image_test[:, :, 0])
-# plt.imshow(np.ones((25,25)))
-# plt.show()
-# print(image_data_set.shape())
 
-##Here would be room for update
+
+        #saveNucleus
+        #savetheimage(image_data_set[:, :, 0, i],filename,savepath=SavePath+'Nucleus\\')
+        #saveActin
+        #savetheimage(image_data_set[:, :, 2, i],filename,savepath=SavePath+'Actin\\')
+
+
+
+
+
+
